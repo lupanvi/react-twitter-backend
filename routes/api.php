c1501917d7343c4e117bb0fa06d9b65f46239b32
Luis Panta <lupanvi@hotmail.com>
2021-04-12 11:11:58 -0500

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -21,17 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-
-	//Route::get('/posts/{post}','PostsController@show');
+	
 	Route::get('/tweets',[TweetsController::class,'index']);
-	Route::post('/tweets',[TweetsController::class, 'store']);
-
-	/*Route::patch('/posts/{post}','PostsController@update');
-	Route::post('/posts','PostsController@store');
-	Route::delete('/posts/{post}','PostsController@destroy');
-	Route::post('/posts/{post}/likes','LikesController@store');
-	Route::post('/posts/{post}/dislike','LikesController@destroy');
-	Route::post('/posts/{post}/comments','CommentsController@store');
-	Route::get('/posts/{post}/comments/all','CommentsController@index');*/
+	Route::post('/tweets',[TweetsController::class, 'store']);	
 
 });
